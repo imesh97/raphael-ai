d603c6bd6252f2982b743654ce2605c50d9c60b0
Imesh Nimsitha <imesh.nimsitha@gmail.com>
2024-05-13 19:52:00 -0400

    added sign in text to button

--- components/shared/SidebarNav.tsx
+++ components/shared/SidebarNav.tsx
@@ -84,7 +84,7 @@ const SidebarNav = () => {
 
           <SignedOut>
             <Button asChild className="button bg-purple-gradient bg-cover">
-              <Link href="/sign-in"></Link>
+              <Link href="/sign-in">Sign In</Link>
             </Button>
           </SignedOut>
         </nav>
